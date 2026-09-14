b9118fd47b30ed07a41dd870ec20b45d2dca5cec
Aman Mishra <aman8349700000@gmail.com>
2026-09-14 23:31:29 +0530

    Add Armstrong number checker in day17.1.c

---
+++ day17.1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, original, digit, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0)
+    {
+        digit = n % 10;
+        sum = sum + (digit * digit * digit);
+        n = n / 10;
+    }
+
+    if (sum == original)
+    {
+        printf("Number is an Armstrong number\n");
+    }
+    else
+    {
+        printf("Number is not an Armstrong number\n");
+    }
+
+    return 0;
+}
