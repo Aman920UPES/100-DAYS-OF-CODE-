f093fde3f82d26e664d9497dd52de6ec64f58bab
Aman Mishra <aman8349700000@gmail.com>
2026-09-14 23:29:03 +0530

    Add binary conversion program in day16.1.c

---
+++ day16.1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, remainder;
+    long long binary = 0, place = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0)
+    {
+        remainder = n % 2;
+        binary = binary + remainder * place;
+        place = place * 10;
+        n = n / 2;
+    }
+
+    printf("Binary representation = %lld\n", binary);
+
+    return 0;
+}
