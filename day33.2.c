bdf4a8a1d563b8194c382e788bebf5123abe178f
Aman Mishra <aman8349700000@gmail.com>
2026-09-15 14:13:14 +0530

    Implement insertion in sorted array
    This program allows the user to insert an element into a sorted array while maintaining the sorted order.

---
+++ day33.2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100];
+    int n, i, element, pos;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements in sorted order:\n", n);
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to insert: ");
+    scanf("%d", &element);
+
+    // Find the correct position
+    pos = 0;
+
+    while(pos < n && arr[pos] < element)
+    {
+        pos++;
+    }
+
+    // Shift elements to the right
+    for(i = n; i > pos; i--)
+    {
+        arr[i] = arr[i - 1];
+    }
+
+    // Insert the element
+    arr[pos] = element;
+    n++;
+
+    printf("Array after insertion:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
